e158c400b3a4b85890f6b56494e5258f502ea585
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-11-27 19:03:00 -0300

    Finished Server Setup

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors';
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
+async function bootstrap() {
+    const fastify = Fastify({ logger: true, });
+
+    await fastify.register(cors, {
+        origin: true
+    });
+
+    fastify.get('/polls/count', async () => {
+        const count = await prisma.poll.count();
+        return { count }
+    })
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' });
+}
+
+bootstrap();
